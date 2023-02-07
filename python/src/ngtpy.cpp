8a9135395b65bced0d6c0718b6e7c730c67fa38c
SDU-L <60847320+SDU-L@users.noreply.github.com>
2023-02-07 11:38:45 +0800

    Update ngtpy.cpp

--- python/src/ngtpy.cpp
+++ python/src/ngtpy.cpp
@@ -125,7 +125,7 @@ public:
     py::array_t<double> objectsmod = np.attr("norm")(objects, "ord"_a = 2, "axis"_a = 1, "keepdims"_a = keepdims);
     objects = objects/objectsmod;
     std::cerr << "运行到ngtpy.cpp：126" << std::endl;  // 测试
-    std::cerr << "objects的类型为 " << type(objects) << std::endl;  // 测试
+    std::cerr << "objects的类型为 " << typeid(objects).name() << std::endl;  // 测试
     std::cerr << objects << std::endl;  // 测试
     //验证
     //增
